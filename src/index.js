82e1208c87650b34a900d23e5e7635bcc35855a8
pagoulid <pagoulid@gmail.com>
2022-11-24 01:30:57 +0200

    storing book data

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import _ from 'lodash';
 import './style.css';
 
+let myLibrary=[]
+
+function Book(title,author,pages){
+  return {title,author,pages}
+}
 /*function component() {
     const element = document.createElement('button');
     element.classList.add('button');
@@ -18,13 +23,24 @@ import './style.css';
     element.classList.add('main')
     return element;
   }*/
+  
+  const container=document.getElementById('books-container');
+  const cardNode=document.querySelector('.data-card');
   const button = document.getElementById('new-book');
   const close = document.querySelector('.close')
+  const submit = document.querySelector('.submit')
   button.addEventListener('click',()=>{show_data_card('open')})
   close.addEventListener('click',()=>{show_data_card('close')})
+  submit.addEventListener('click',(e)=>{
+    e.preventDefault()
+    addBook()
+  })
+  
+
+  
 
   function show_data_card(msg){
-    const cardNode=document.querySelector('.data-card');
+    
     const isVisible= cardNode.classList.contains('visible-data-card');
     const isHidden= cardNode.classList.contains('hidden-data-card');
 
@@ -43,5 +59,36 @@ import './style.css';
     } 
 
   }
+
+  function addBook(){
+
+    
+    cardNode.classList.replace('visible-data-card','hidden-data-card');
+    
+    const titleNode=document.getElementById('book-title');
+    const authorNode=document.getElementById('book-author');
+    const pagesNode=document.getElementById('book-pages');
+
+    const title=titleNode.value;
+    const author=authorNode.value;
+    const pages=pagesNode.value;
+
+   
+
+    const newBook =Book(title,author,pages);
+    myLibrary.push(newBook)
+    console.log(myLibrary)
+    if(myLibrary.length>0){
+
+      container.innerHTML= `${myLibrary.map((book) => `<ul><li><p>Title:${book.title}</p><p>Author:${book.author}</p><p>Pages:${book.pages}</p></li></ul>`).join('')}`
+
+
+    }
+   
+    titleNode.value=''
+    authorNode.value=''
+    pagesNode.value=''
+  }
+  
   
   //document.body.appendChild(main()).appendChild(component());
