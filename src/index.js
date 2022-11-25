a56e5cde9ad4f669ebe1ed573978775017752830
pagoulid <pagoulid@gmail.com>
2022-11-25 12:01:04 +0200

    structuring code

--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
 import _ from 'lodash';
 import {replace,toggle,getVal,clearText} from './functions.js'
 import {get_data_card_nodes,set_data_card_nodes,get_entry_data} from './data.js'
-
 import './style.css';
 
-let myLibrary=[]
+/* GLOBALS */
+let myLibrary=[] 
 
-let updateTarget=''
-let gmsg=''
-let count=0 
+let updateTarget='' // store target which is going to be updated
+let gmsg=''  // store msg for submission card (open/close/update)
+let count=0  //count books
+/* GLOBALS */
 
+/*BOOK*/ 
 function Book(id,title,author,pages){
   return {id,title,author,pages}
 }
 
 function addBook(){
 
-  replace(cardNode,{primary:'visible-data-card',replacement:'hidden-data-card'})
-  const Nodes= get_data_card_nodes() //[titleNode,authorNode,pagesNode]
-
+  replace(cardNode,{primary:'visible-data-card',replacement:'hidden-data-card'})// close after submission
+  
+  const Nodes= get_data_card_nodes() //[titleNode,authorNode,pagesNode] -> get input data
   const title=Nodes[0].value;
   const author=Nodes[1].value;
   const pages= Nodes[2].value;
@@ -27,8 +29,8 @@ function addBook(){
 
  addBookToLibrary(count,title,author,pages);
  displayToDOM();
- addUpdateEventListener();
- clearText(Nodes);
+ addEntriesEventListener();
+ clearText(Nodes);// clear input text from submission card
   
 }
 
@@ -41,16 +43,16 @@ function deleteBook(id){
   deleteBookFromLibrary(id,count)
   
   displayToDOM()
-  addUpdateEventListener()
-  console.log(myLibrary)
+  addEntriesEventListener()
+  
 }
 
 
 function deleteBookFromLibrary(id){
-  myLibrary.splice(id-1,1)
+  myLibrary.splice(id-1,1) // remove book with targeted id
+  
   count=count-1
-
-  for(let i=0;i<=count-1;i++){
+  for(let i=0;i<=count-1;i++){ // rearrange ids to books
     myLibrary[i].id=i+1
   }
 }
@@ -59,20 +61,21 @@ function updateBook(){
     
   replace(cardNode,{primary:'visible-data-card',replacement:'hidden-data-card'})
   
-  const Nodes= get_data_card_nodes() //[titleNode,authorNode,pagesNode]
+  const Nodes= get_data_card_nodes() //[titleNode,authorNode,pagesNode]-> get input data
   const title=Nodes[0].value;
   const author=Nodes[1].value;
   const pages= Nodes[2].value;
 
-  myLibrary[updateTarget-1].title=title // Note updateTarget is stored target from update click button
+  myLibrary[updateTarget-1].title=title 
   myLibrary[updateTarget-1].author=author
   myLibrary[updateTarget-1].pages=pages
   
   displayToDOM()
-  addUpdateEventListener() // Also update event listeners
+  addEntriesEventListener() 
   clearText(Nodes);
   gmsg='' // To avoid duplicate update check on next onclick for update
 }
+/*BOOK*/ 
 
 function displayToDOM(){
   container.innerHTML= `${myLibrary.map((book) => `<ul id="${book.id}">
@@ -84,21 +87,7 @@ function displayToDOM(){
     </ul>`).join('')}`
 }
 
-function addUpdateEventListener(){
-  myLibrary.forEach((book)=>{
-    const updateButtonNode=document.getElementById(book.id)
-    updateButtonNode.addEventListener('click',(e)=>{
-      const clickedButton= e.target.textContent;
-      if(clickedButton=='Update'){
-        show_data_card('update',e.currentTarget.id);
-      }
-      else{ //delete operation
-        console.log(e.currentTarget.id)
-        deleteBook(e.currentTarget.id)
-      }   
-    })
-  })  
-}
+
 
   const container=document.getElementById('books-container');
   const cardNode=document.querySelector('.data-card');
@@ -106,13 +95,33 @@ function addUpdateEventListener(){
   const close = document.querySelector('.close')
   const submit = document.querySelector('.submit')
 
+    /*EVENT LISTENERS*/
   button.addEventListener('click',()=>{show_data_card('open',null)})
   close.addEventListener('click',()=>{show_data_card('close',null)})
+
   submit.addEventListener('click',(e)=>{
     e.preventDefault()
     submit_data()
   })
-  
+
+  function addEntriesEventListener(){
+    myLibrary.forEach((book)=>{
+      const updateButtonNode=document.getElementById(book.id)
+      
+      updateButtonNode.addEventListener('click',(e)=>{
+      const clickedButton= e.target.textContent; // target is the button user clicked
+        if(clickedButton=='Update'){
+          show_data_card('update',e.currentTarget.id);
+        }
+        else{ //delete operation
+          
+          deleteBook(e.currentTarget.id) // currentTarget is the card user clicked on
+        }   
+      })
+    })  
+  }
+  /*EVENT LISTENERS*/
+  /*SUBMIT CARD*/ 
   function submit_data(){
     if(gmsg=='open'){ // what button did user clicked on (update or + to add??)
       addBook()
@@ -123,26 +132,33 @@ function addUpdateEventListener(){
   }
 
   function show_data_card(msg,target){ // need target for to get book id for update
-    const dup_update_flag = gmsg // Keep old msg , if old msg and new msg are both update then do not do nothing
+    const prev_msg = gmsg // Keep old msg , if old msg and new msg are both update then do not do nothing
     gmsg=msg
     
     const isVisible= cardNode.classList.contains('visible-data-card');
-    const isHidden= cardNode.classList.contains('hidden-data-card');
+    //const isHidden= cardNode.classList.contains('hidden-data-card');
 
     const flagCondition=cardNode.classList.contains('flag')
-    const openCondition= gmsg==('open' || 'update') && isVisible;
-    const closeCondition= gmsg=='close' && isHidden;
+    const openCondition=gmsg=='open'// (gmsg=='open' || gmsg=='update') && isVisible;
     const updateCondition= gmsg=='update';
-    const prevUpdateCondition = dup_update_flag=='update'
-    const updateDupCondition= !(prevUpdateCondition && updateCondition);
+    const prevUpdateCondition = prev_msg=='update';
+    const closeCondition= gmsg=='close';
+    const visibleCondition=(openCondition || updateCondition) && isVisible;
+    const updateDupCondition= !(prevUpdateCondition && updateCondition); // avoid duplicates clicks
 
-    if(!(openCondition || closeCondition)){
+    if(closeCondition){
+      const Nodes=get_data_card_nodes()
+      Nodes[0].value=''
+      Nodes[1].value=''
+      Nodes[2].value=''
+    }
+    if(!(visibleCondition)){
 
       if(flagCondition){
         replace(cardNode,{primary:'flag',replacement:'visible-data-card'})
       }
       else{
-        if(updateDupCondition){// In case new/old msg is update
+        if(updateDupCondition){
           toggle(cardNode,['hidden-data-card','visible-data-card'])
         }
         
@@ -153,10 +169,9 @@ function addUpdateEventListener(){
             updateTarget=target // store target to global variable to use it when handling update submission
         }
       }
-        
     }
   } 
-
+/*SUBMIT CARD*/
 
 
   
